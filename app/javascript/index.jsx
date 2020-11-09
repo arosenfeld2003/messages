5424e5cdd3a7e00033c6eb9fa94f753efab3f425
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-11-09 11:28:23 -0800

    added login form

--- app/javascript/index.jsx
+++ app/javascript/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "../components/Home";
-import SignUpPage from "../pages/sign-up-page/sign-up-page";
+import Home from "./components/Home";
+import SignUpPage from "./pages/sign-up-page/sign-up-page";
+import SignInPage from "./pages/sign-in-page/sign-in-page";
 
 const App = () => {
   return <div className="main">
@@ -9,6 +10,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/signup" component={SignUpPage} />
+        <Route path="/login" component={SignInPage} />
       </Switch>
     </Router>
   </div>
--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import App from "../routes/index";
+import App from "../index";
 
 document.addEventListener("DOMContentLoaded", () => {
   render(
