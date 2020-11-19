8dea1ec9d0ca23e6a030462509cf99c516c9a6b4
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-11-19 11:42:55 -0800

    added logged_in route for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/logged_in', to: 'sessions#logged_in?'
   devise_for :users, controllers: { registrations: "registrations", sessions: 'sessions'}
   root 'login#index'
 
