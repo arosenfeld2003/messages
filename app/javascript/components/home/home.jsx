12d1a75f112de80fe059a8ed7a6a302caebf778b
Kseniia Anishchenko <kseninavoi@gmail.com>
2020-12-19 14:34:04 -0800

    added cards

--- app/javascript/components/home/home.jsx
+++ app/javascript/components/home/home.jsx
@@ -17,12 +17,31 @@ const Home = (props) => {
     <div className="main-content">
       <div className="container">
         <div className="row">
-          <div className="col-3">
-            <p class="main-text">{currentUser.email}</p>
+          <div className="col-3 p-3">
+            <div class="card bg-white">
+              <img class="card-img-top" src="..." alt="Card image cap" />
+              <div class="card-body">
+                <h5 class="card-title">{currentUser.email}</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Your posts</li>
+                <li class="list-group-item">Followers</li>
+                <li class="list-group-item">Following</li>
+              </ul>
+            </div>
           </div>
-          <div className="col">
-            
-            Posts
+          <div className="col p-3">
+          <div class="col-sm">
+            <h3>Newsfeed</h3>
+            <div class="card bg-white">
+              <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="card-link text-secondary">Read more...</a>
+              </div>
+            </div>
+          </div>
           </div>
         </div>
       </div>
--- db/migrate/20201209195837_drop_table_token_lists.rb
+++
@@ -1,5 +0,0 @@
-class DropTableTokenLists < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :token_lists
-  end
-end
