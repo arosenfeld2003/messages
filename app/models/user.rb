1c497ce7fb6dd20291ab98efd560fa1400da643d
Alex Rosenfeld <arosenfeld2003@mac.com>
2021-01-16 21:24:33 -0800

    install rspec and add Friends table with references

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets, primary_key: :handle_id
+
+  has_many :followers, foreign_key: :follower_id , class_name: "Friend"
+  has_many :followed, through: :followers
+  
+  has_many :followed, foreign_key: :followed_id, class_name: "Friend"
+  has_many :followers, through: :followed
+
 end
