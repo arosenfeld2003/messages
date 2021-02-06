4b74bf4c7148d9c87c1b30a0b0cb19f1539bf5a9
Kseniia Anishchenko <kseninavoi@gmail.com>
2021-02-06 14:57:05 -0800

    chenged baground  of welcome page

--- app/javascript/components/welcome/welcome.jsx
+++ app/javascript/components/welcome/welcome.jsx
@@ -7,10 +7,9 @@ import "./welcome.scss";
 const Welcome = (props) => {
 
   return <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center welcome-page">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
+    <div className="container">
       <div className="row">
-        <div className="col-6">
-          <div className="container secondary-color">
+        <div className="col-7">
             <h1 className="display-3">Welcome</h1>
             <p className="font-weight-light">
             NewsPaper is a 'microblogging' system that allows you to send and receive short posts. Posts can be up to 140 characters long and can include links to relevant websites and resources.
@@ -30,7 +29,6 @@ const Welcome = (props) => {
             >
               Login
             </Link>
-          </div>
         </div>
       </div>
     </div>
