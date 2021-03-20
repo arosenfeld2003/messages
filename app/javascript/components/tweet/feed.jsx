ce5a5cb6d0ef0780067415477730e53f7c5ae4ea
Kseniia Anishchenko <kseninavoi@gmail.com>
2021-03-20 15:29:43 -0700

    added total comments

--- app/javascript/components/tweet/feed.jsx
+++ app/javascript/components/tweet/feed.jsx
@@ -6,6 +6,7 @@ import CommentsList from "../comments-list/comments-list";
 
 const Feed = (props) => {
   const {currentUser, fetchUserFeed, userFeed} = props;
+
   const loadUserFeed = function () {
     fetchUserFeed(currentUser) || [];
   }
@@ -27,12 +28,6 @@ const Feed = (props) => {
                 <p className="card-text">{tweet.body}</p>
               </div>
               <div className="card-footer text-muted text-right">
-                <button type="button" className="btn btn-link text-primary">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chat-square-text" viewBox="0 0 16 16">
-                    <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-                    <path d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
-                  </svg>
-                </button>
 
                 <button type="button" className="btn btn-link text-info">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter" viewBox="0 0 16 16">
