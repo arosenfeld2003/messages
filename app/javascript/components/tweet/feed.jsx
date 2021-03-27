b3010c6e6160b422df8c73e7cf8a79db39d90f83
Kseniia Anishchenko <kseninavoi@gmail.com>
2021-03-27 16:38:24 -0700

    refactored likes component

--- app/javascript/components/tweet/feed.jsx
+++ app/javascript/components/tweet/feed.jsx
@@ -31,18 +31,27 @@ const Feed = (props) => {
           userFeed[0] !=  undefined ? userFeed.map((tweet, index) => (
             <div key={index}>
               <div className="card">
-                <div className="card-header card-title"><strong>@{tweet.handle}</strong>, <small>{tweet.created_at.slice(0, 10)}</small></div>
+                <div className="card-header card-title">
+                  <strong>@{tweet.handle}</strong>, <small>{tweet.created_at.slice(0, 10)}</small>
+                </div>
                 <div className="card-body">
                   <p className="card-text">{tweet.body}</p>
                 </div>
                 <div className="card-footer text-muted text-right">
-
-                  <FavoriteTweetButton tweet={tweet} user={currentUser} />
-                  <Likes tweet={tweet}/>
-
-                  {currentUser.handle === tweet.handle &&
-                    <DeleteTweetButton tweet={tweet}/>
-                  }
+                  <div className="row">
+                    <div className="col text-right d-flex justify-content-end">
+                      <button type="button" className="btn btn-link text-info retweet">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter" viewBox="0 0 16 16">
+                          <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+                        </svg>
+                      </button>
+                      <FavoriteTweetButton tweet={tweet} user={currentUser} />
+                      {currentUser.handle === tweet.handle &&
+                        <DeleteTweetButton tweet={tweet}/>
+                      }
+                    </div>
+                    
+                  </div>
                   <CommentsList
                     tweet={tweet}
                     currentUser={currentUser}
