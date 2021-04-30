a69f90acfc50e4ad1d83dfbedd96eb49cc15be2e
Alex Rosenfeld <arosenfeld2003@mac.com>
2021-04-30 13:27:33 -0700

    change title of app to messages

--- app/javascript/components/welcome/welcome.jsx
+++ app/javascript/components/welcome/welcome.jsx
@@ -12,7 +12,7 @@ const Welcome = (props) => {
         <div className="col-7">
             <h1 className="display-3">Welcome</h1>
             <p className="font-weight-light">
-            NewsPaper is a 'microblogging' system that allows you to send and receive short posts. Posts can be up to 140 characters long and can include links to relevant websites and resources.
+            Messages is a Twitter-like 'microblogging' system that allows you to send and receive short posts. Posts can be up to 140 characters long and can include links to relevant websites and resources.
             </p>
             <hr className="my-4" />
             <Link
